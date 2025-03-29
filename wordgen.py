e3994c88fc5c1d4f2ad9902aeb353158d727bdf0
Ant <ananthajit5d@gmail.com>
2025-03-28 18:20:44 -0700

    I guess I'm getting whitespaces now

--- wordgen.py
+++ wordgen.py
@@ -2,6 +2,7 @@ from nltk.util import ngrams
 from nltk.lm.preprocessing import padded_everygram_pipeline # wtf does this mean halp
 from nltk.lm import KneserNeyInterpolated
 import sys
+from random import sample
 print(sys.getdefaultencoding())
 # sys.setdefaultencoding("utf-8")
 
@@ -13,12 +14,13 @@ with open("data/data.txt", "r", encoding="utf-8") as dunmeris:
     # ald
     wordlist = [word.strip() for word in dunmeris.readlines()]
 
-text = "".join(wordlist)
-
+text = " ".join(sample(wordlist, len(wordlist)))
+print(text)
 n = 3
 
 train, vocab = padded_everygram_pipeline(n, text.split())
 model = KneserNeyInterpolated(n) 
 model.fit(train, vocab)
-print(model.generate(num_words = 10, random_seed=5283475))
+# print(model.generate(num_words = 10, random_seed=5283475))
+print(model.generate(num_words = 10))
 
